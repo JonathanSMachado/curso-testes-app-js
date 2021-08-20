1321f3de9f9b68c857b404fa9196ffac34075cf1
Jonathan S. Machado <jonathansmachado@gmail.com>
2021-08-19 23:13:26 -0300

    Add class Cart and your tests

---
+++ src/lib/Cart.js
@@ -0,0 +1,78 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+import Dinero from 'dinero.js';
+
+const Money = Dinero;
+Money.defaultCurrency = 'BRL';
+Money.defaultPrecision = 2;
+
+const calculatePercentageDiscount = (amount, item) => {
+  if (item.quantity > item.condition.minimum) {
+    return amount.percentage(item.condition.percentage);
+  }
+
+  return Money({ amount: 0 });
+};
+
+const calculateQuantityDiscount = (amount, item) => {
+  const isEven = item.quantity % 2 === 0;
+
+  if (item.quantity > item.condition.quantity) {
+    return amount.percentage(isEven ? 50 : 40);
+  }
+
+  return Money({ amount: 0 });
+};
+
+export default class Cart {
+  items = [];
+
+  add(item) {
+    const itemToFind = { product: item.product };
+
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+
+    this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      const amount = Money({ amount: item.quantity * item.product.price });
+      let discount = Money({ amount: 0 });
+
+      if (item.condition?.percentage) {
+        discount = calculatePercentageDiscount(amount, item);
+      } else if (item.condition?.quantity) {
+        discount = calculateQuantityDiscount(amount, item);
+      }
+
+      return acc.add(amount).subtract(discount);
+    }, Money({ amount: 0 }));
+  }
+
+  summary() {
+    const total = this.getTotal().getAmount();
+    const items = this.items;
+
+    return {
+      total,
+      items,
+    };
+  }
+
+  checkout() {
+    const { total, items } = this.summary();
+    this.items = [];
+
+    return {
+      total,
+      items,
+    };
+  }
+}
---
+++ src/lib/Cart.spec.js
@@ -0,0 +1,176 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+  let cart = null;
+  const product = {
+    title: 'Adidas running shoes - men',
+    price: 35388,
+  };
+
+  const product2 = {
+    title: 'Adidas running shoes - women',
+    price: 47199,
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly instance', () => {
+      expect(cart.getTotal().getAmount()).toEqual(0);
+    });
+
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal().getAmount()).toEqual(47199);
+    });
+  });
+
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('should return an object with the total and the list of items when summary() is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+    });
+
+    it('should reset the cart when checkout() is called', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal().getAmount()).toEqual(0);
+    });
+  });
+
+  describe('special conditions', () => {
+    it('should apply percentage discount quantity above minimum is passed', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 3,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(74315);
+    });
+
+    it('should NOT apply percentage discount quantity is below or equals minimum', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 2,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should apply quantity discount for even quantities', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 4,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should NOT apply quantity discount for even quantities when condition is not met', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(35388);
+    });
+
+    it('should apply quantity discount for odd quantities', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 5,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(106164);
+    });
+  });
+});
