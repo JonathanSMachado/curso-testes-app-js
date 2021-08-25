f09d98f9033c52447d86945449692a2add8bf558
Jonathan S. Machado <jonathansmachado@gmail.com>
2021-08-24 22:50:00 -0300

    Adiciondo miragejs-starter-kit

--- Modulo_2/pages/index.vue
+++ Modulo_2/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="my-8">
-    <div class="container mx-auto px-6">
+    <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
       <div
@@ -21,20 +21,28 @@
         />
       </div>
     </div>
+    <h3 v-else class="text-2xl text-center text-gray-700">
+      {{ errorMessage }}
+    </h3>
   </main>
 </template>
 
 <script>
-import ProductCard from '@/components/ProductCard';
+import ProductCard from "@/components/ProductCard";
 export default {
   components: { ProductCard },
   data() {
     return {
       products: [],
+      errorMessage: "",
     };
   },
   async created() {
-    this.products = (await this.$axios.get('/api/products')).data.products;
+    try {
+      this.products = (await this.$axios.get("/api/products")).data.products;
+    } catch (error) {
+      this.errorMessage = "Problemas ao carregar a lista!";
+    }
   },
 };
 </script>
