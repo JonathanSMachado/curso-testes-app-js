d297233922158803ecb639ecb628991168a6c23f
Jonathan S. Machado <jonathansmachado@gmail.com>
2021-08-31 22:27:08 -0300

    Adicionado CartManager para gerenciamento simples de estado

---
+++ Modulo_2/watch-store/managers/CartManager.js
@@ -0,0 +1,65 @@
+import Vue from 'vue';
+
+const initialState = {
+  open: false,
+  items: [],
+};
+
+export class CartManager {
+  state;
+
+  constructor() {
+    this.state = Vue.observable(initialState);
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  open() {
+    this.state.open = true;
+
+    return this.getState();
+  }
+
+  close() {
+    this.state.open = false;
+
+    return this.getState();
+  }
+
+  isProductInTheCart(product) {
+    return !!this.state.items.find(({ id }) => id === product.id);
+  }
+
+  addProduct(product) {
+    if (!this.isProductInTheCart(product)) {
+      this.state.items.push(product);
+    }
+
+    return this.getState();
+  }
+
+  removeProduct(productId) {
+    this.state.items = this.state.items.filter(({ id }) => id !== productId);
+
+    return this.getState();
+  }
+
+  clearProducts() {
+    this.state.items = [];
+
+    return this.getState();
+  }
+
+  clearCart() {
+    this.clearProducts();
+    this.close();
+
+    return this.getState();
+  }
+
+  hasProductsInTheCart() {
+    return this.state.items.length > 0;
+  }
+}
