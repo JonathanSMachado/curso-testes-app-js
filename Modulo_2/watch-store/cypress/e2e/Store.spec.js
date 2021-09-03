0b7252c9beacba3f82a6db0ddebb9c2885c1bbe9
Jonathan S. Machado <jonathansmachado@gmail.com>
2021-09-02 22:11:38 -0300

    Configurado MirageJS para funcionar com Cypress

--- Modulo_2/watch-store/cypress/e2e/Store.spec.js
+++ Modulo_2/watch-store/cypress/e2e/Store.spec.js
@@ -1,9 +1,20 @@
+import { makeServer } from '../../miragejs/server';
+
 context('Store e2e', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should visit the store', () => {
     cy.visit('http://localhost:3000');
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
-    cy.get('body').contains('25 Products');
   });
 });
